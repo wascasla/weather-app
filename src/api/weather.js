b59620213a07976b1eb2e76ad983da6c0c6834dc
Walter <wascasla@gmail.com>
2021-08-14 19:55:29 -0300

    Add function to get weather on selected city at Inicio component

--- src/api/weather.js
+++ src/api/weather.js
@@ -2,6 +2,7 @@ import api from "./api";
 
 const key = process.env.REACT_APP_KEY_WEATHER_API;
 
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
   getCurrentWeather: function (latitude, longitude) {
     return api.get(`weather/?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`);
